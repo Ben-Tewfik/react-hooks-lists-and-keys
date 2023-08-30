49ae5b0f3b000983c606c2bcbbc83934eac6e525
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-08-30 22:11:17 +0200

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,17 +8,38 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
+  const colorElement = colors.map(color => {
+    return (
+      <li key={color} style={{ color: color }}>
+        {color}
+      </li>
+    );
+  });
+
+  const users = [
+    { id: 1, firstName: "Duane", lastName: "Watson" },
+    { id: 2, firstName: "Duane", lastName: "Johnson" },
+  ];
+
+  const userArr = users.map(user => {
+    return (
+      <p key={user.id}>
+        My name is {user.firstName} {user.lastName}{" "}
+      </p>
+    );
+  });
+
+  const fib = [0, 1, 1, 2, 3, 5];
+  const fibNumber = fib.map((number, index) => {
+    return <p key={index}>{number}</p>;
+  });
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
+      <ol>{colorElement}</ol>
+      <div>{userArr}</div>
+      <div>{fibNumber}</div>
     </div>
   );
 }
